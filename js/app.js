50e6772ac6bd213534afce0c369fedf4b31a3e0c
gabrielbazan7 <gabrielbazan7@hotmail.com>
2015-03-12 14:53:42 -0300

    refresh

--- js/app.js
+++ js/app.js
@@ -2,21 +2,16 @@
 
 var myApp = angular.module('myApp', ['ngRoute']);
 myApp.controller('searchController', ['$scope', '$http', function($scope, $http) {
-    $scope.searchsArray = [{
-        name: 'artist'
-    }, {
-        name: 'album'
-    }];
-    $scope.searchItem = '';
     $scope.submit = (function() {
-        varArtist = $('.search-input').val();
+        varArtistOrAlbum = $('.search-input').val();
         $http.get('https://api.spotify.com/v1/search', {
             params: {
-                q: varArtist,
-                type: $scope.searchItem.name
+                q: varArtistOrAlbum,
+                type: "album,artist"
             }
         }).success(function(data) {
             $scope.searchs = data;
+            $scope.viewEnable=true;
         });
     })
 }])
