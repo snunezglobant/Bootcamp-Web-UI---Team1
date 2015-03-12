6fcb98ba4873f1b8dcf54c599aaeca8e30c30131
Franco Gallardo Grazio <franco.massa@hotmail.com>
2015-03-12 00:19:32 -0300

    Improved layout, fixed unobtrusive topbar

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
-$(window).on('scroll', {
-    previousTop: 0
-  },
-  function() {
-    var currentTop = $(window).scrollTop();
-    var topbar = $('.topbar');
+(function() {
+  'use strict'
 
-    if (topbar.css('position') === 'absolute') {
-      if (currentTop < this.previousTop) {
-        // scrolling up
-        if (currentTop < topbar.position().top) {
-          topbar.css('top', currentTop + 'px');
-        }
-      }
-      else {
-        // scrolling down
-        if (currentTop > parseInt(topbar.position().top + topbar.height())) {
-          topbar.css('top', parseInt(currentTop - topbar.height() - 20) + 'px');
-        }
-      }
+  /*
+  * When viewing the page in a mobile device, the top bar is automatically hidden
+  * when the user scrolls down, and is brought back as soon as the user scrolls up.
+  * This way, screen real estate is maximized without affecting usability.
+  */
+  $(window).on('scroll', {
+      previousTop: 0
+    },
+    function scrollTopbar() {
+      var currentTop = $(window).scrollTop();
+      var topbar = $('.topbar');
 
-      this.previousTop = currentTop;
+      if (window.matchMedia('(max-width: 768px)').matches) {
+        if (currentTop < this.previousTop) {
+          // scrolling up
+          if (currentTop > 0) {
+            topbar.addClass('animate visible');
+          }
+          else {
+            topbar.removeClass('fixed animate visible');
+          }
+        }
+        else {
+          // scrolling down
+          if (currentTop > topbar.height()) {
+            // hide the animation when transitioning from position: absolute to position: fixed
+            if (topbar.hasClass('fixed')) {
+              topbar.addClass('animate');
+            }
+            else {
+              topbar.addClass('fixed');
+            }
+            topbar.removeClass('visible');
+          }
+        }
+
+        this.previousTop = currentTop;
+      }
+      console.log(topbar.position().top);
     }
-  }
-);
+  );
+
+})();
